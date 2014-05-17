fb759ce61f5ccfb0abdfb7347e829330515f67a3
Eugene Koontz <ekoontz@hiro-tan.org>
2014-05-17 11:14:22 -0700

    check if things are running (client or server) before starting

--- run.sh
+++ run.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
-jruby web_app.rb &
+# if web_app.rb is already running, use that rather than trying to restart.
+if [[ -z "$(ps -ef | grep java | grep web_app.rb | grep -v grep | awk '{print $2}')" ]]; then
+    echo "web_app.rb not running: starting now.."
+    jruby web_app.rb &
+else
+    echo "web_app.rb already running: not starting."
+fi
 
-jconsole localhost:9999 &
+if [[ -z "$(ps -ef | grep jconsole | grep -v grep | awk '{print $2}')" ]]; then
+    echo "jconsole not running: starting now.."
+    jconsole localhost:9999 &
+else
+    echo "jconsole already running: not starting."
+fi
 
 echo "All set up. in your browser, go to http://localhost:4567 and hit refresh a few times"
 echo " and then watch the resulting increments in jconsole -> MBeans -> DefaultDomain ->"
